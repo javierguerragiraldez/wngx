ad793c1393397e4aac1ff3dae12bc7da15c58765
Javier Guerra <javier.guerra@konghq.com>
2020-04-06 14:19:18 -0500

    remove commented code dead ends

--- ngx_http_wasm_module.c
+++ ngx_http_wasm_module.c
@@ -9,31 +9,11 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-// static char *ngx_http_wasm(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static ngx_int_t ngx_http_wasm_postconf(ngx_conf_t *cf);
 static void *ngx_http_wasm_create_loc_conf(ngx_conf_t *cf);
 static char *ngx_http_wasm_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+static ngx_int_t ngx_http_wasm_postconf(ngx_conf_t *cf);
 static ngx_int_t ngx_http_wasm_init_proc(ngx_cycle_t *cycle);
 
-// static ngx_int_t ngx_http_wasm_handler(ngx_http_request_t *r);
-
-static ngx_int_t ngx_http_wasm_rewrite_handler(ngx_http_request_t *r);
-static ngx_int_t ngx_http_wasm_access_handler(ngx_http_request_t *r);
-static ngx_int_t ngx_http_wasm_content_hanlder(ngx_http_request_t *r);
-static ngx_int_t ngx_http_wasm_log_handler(ngx_http_request_t *r);
-
-// static ngx_int_t ngx_http_wasm_create_request(ngx_http_request_t *r);
-// static ngx_int_t ngx_http_wasm_reinit_request(ngx_http_request_t *r);
-// static ngx_int_t ngx_http_wasm_process_header(ngx_http_request_t *r);
-// static void ngx_http_wasm_abort_request(ngx_http_request_t *r);
-// static void ngx_http_wasm_finalize_request(ngx_http_request_t *r, ngx_int_t i);
-
-// static ngx_int_t ngx_http_stub_status_handler(ngx_http_request_t *r);
-// static ngx_int_t ngx_http_stub_status_variable(ngx_http_request_t *r,
-//     ngx_http_variable_value_t *v, uintptr_t data);
-// static ngx_int_t ngx_http_stub_status_add_variables(ngx_conf_t *cf);
-// static char *ngx_http_set_stub_status(ngx_conf_t *cf, ngx_command_t *cmd,
-//     void *conf);
 
 typedef struct {
     ngx_str_t  wasm_path;
@@ -107,67 +87,6 @@ static char *ngx_http_wasm_merge_loc_conf(ngx_conf_t *cf, void *parent, void *ch
     return NGX_CONF_OK;
 }
 
-// static ngx_int_t ngx_http_wasm_handler(ngx_http_request_t *r) {
-//     ngx_http_upstream_t       *u;
-//     ngx_http_wasm_loc_conf_t  *wlcf;
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "on handler");
-//
-//     wlcf = ngx_http_get_module_loc_conf(r, ngx_http_wasm_module);
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-//                   "load wasm: %*s", wlcf->wasm_path.len, wlcf->wasm_path.data);
-//
-// //     u = r->upstream;
-//     u = ngx_pcalloc(r->pool, sizeof(ngx_http_upstream_t));
-//     if (u == NULL) {
-//         ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "u == NULL");
-//         return NGX_HTTP_INTERNAL_SERVER_ERROR;
-//     }
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "p1");
-//     u->peer.log = r->connection->log;
-//     u->peer.log_error = NGX_ERROR_ERR;
-//     u->output.tag = (ngx_buf_tag_t) &ngx_http_wasm_module;
-// //     u->conf = wlcf;
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "p2");
-//     u->create_request = ngx_http_wasm_create_request;
-//     u->reinit_request = ngx_http_wasm_reinit_request;
-//     u->process_header = ngx_http_wasm_process_header;
-//     u->abort_request = ngx_http_wasm_abort_request;
-//     u->finalize_request = ngx_http_wasm_finalize_request;
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "p3");
-//     r->upstream = u;
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "p4");
-//     ngx_int_t rc = ngx_http_read_client_request_body(r, ngx_http_upstream_init);
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "p5 (%ld)", rc);
-//     if (rc >= NGX_HTTP_SPECIAL_RESPONSE) {
-//         return rc;
-//     }
-//
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "p6");
-//     return NGX_OK;
-// }
-
-// static char *ngx_http_wasm(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
-//     (void)cmd; (void)conf;
-//     ngx_log_debug(NGX_LOG_DEBUG, cf->log, 0, "in conf");
-//
-//     char *r = ngx_conf_set_str_slot(cf, cmd, conf);
-//     if (r != NGX_CONF_OK) {
-//         ngx_log_debug(NGX_LOG_DEBUG, cf->log, 0, "not ok setstrslot: %s", r);
-//         return r;
-//     }
-//
-// //     ngx_http_core_loc_conf_t *clcf;
-// //     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-// //     clcf->handler = ngx_http_wasm_handler;
-//
-//     return NGX_CONF_OK;
-// }
 
 
 #define r_log_debug(...) ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, __VA_ARGS__)
@@ -286,26 +205,3 @@ ngx_int_t ngx_http_wasm_init_proc ( ngx_cycle_t* cycle ) {
 
     return NGX_OK;
 }
-
-// ngx_int_t ngx_http_wasm_create_request ( ngx_http_request_t* r ) {
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "create request");
-//     return NGX_OK;
-// }
-//
-// ngx_int_t ngx_http_wasm_reinit_request ( ngx_http_request_t* r ) {
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "reinit request");
-//     return NGX_OK;
-// }
-//
-// ngx_int_t ngx_http_wasm_process_header ( ngx_http_request_t* r ) {
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "process header");
-//     return NGX_OK;
-// }
-//
-// void ngx_http_wasm_abort_request ( ngx_http_request_t* r ) {
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "abort request");
-// }
-//
-// void ngx_http_wasm_finalize_request ( ngx_http_request_t* r, ngx_int_t i ) {
-//     ngx_log_debug(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "finalize request: %d", i);
-// }
