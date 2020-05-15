a362c18f42fb2291e3ddebbd012aecae49f0e24c
Javier Guerra <javier.guerra@konghq.com>
2020-05-15 09:10:52 -0500

    slightly better futures, still fightling with nesting continuations

--- async_cpp.cpp
+++ async_cpp.cpp
@@ -26,6 +26,69 @@ uint32_t on_callback(struct wngx_callback_pun *pun) {
 }
 
 
+class Future;
+struct base_callbacker {
+    virtual void set_future(Future _f) = 0;
+};
+
+
+class Future {
+public:
+    struct base_callbacker *cb;
+    std::function<void*(void*)> func;
+
+//     Future(struct base_callbacker *_cb) : cb(_cb), f(noop) {cb->set_future(*this); }
+    Future(struct base_callbacker *_cb, std::function<void*(void*)>_f = noop) : cb(_cb), func(_f) {cb->set_future(*this);};
+
+    static void* noop(void *p) {
+        log_k ("nop");
+        return p;
+    }
+
+    const Future then(std::function<void*(void*)> _f) const {
+        log_k("setting function")
+
+        auto fw = [=](void *p){
+            return _f(func(p));
+        };
+
+        return Future(cb, fw);
+    };
+};
+
+template <typename P>
+struct callbacker : base_callbacker {
+    P punned;
+    Future fut;
+
+    callbacker(P &&in) : punned(in), fut(this) {}
+
+    static void callback(void *vp) {
+        log_k("callbacker.callback");
+        callbacker<P>* cb = (callbacker<P>*)vp;
+        log_k("got cb");
+        cb->fut.func(&cb->punned);
+        log_k("done future");
+    }
+
+    virtual void set_future(Future _f) override {
+        fut = _f; }
+};
+
+static Future subreq_f(std::string &&uri, std::string &&args = std::string()) {
+
+    auto cb = new callbacker<wngx_subrequest_params>({
+            .uri = (wngx_str){ .d = uri.data(), .len = (uint32_t)uri.size() },
+            .args = (wngx_str){ .d = args.data(), .len = (uint32_t)args.size() },
+        });
+
+    cb->punned.callback = cb->callback;
+    wngx_subrequest(&cb->punned);
+    log_k("requested");
+
+    return cb->fut;
+}
+
 struct subreq {
     wngx_subrequest_params parms;
     std::function<void(wngx_subrequest_params*)> f;
@@ -68,7 +131,7 @@ struct subreq {
 void on_content(void) {
     log_k("content!");
 
-    subreq::q("http://example.com").then([](auto *p){ (void)p; log_k("lambda callback"); });
+    subreq_f("http://example.com").then([](void *p){ log_k("future callback"); return p; });
 
     log_k("queued...");
 }
